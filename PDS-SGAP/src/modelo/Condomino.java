b3d399bff209661af18c1a7c805f0ed19ff8d0f7
Wilson Marques <wilsonmarquesg12@gmail.com>
2015-10-27 15:31:54 -0400

    Administrador inicio

--- PDS-SGAP/src/modelo/Condomino.java
+++ PDS-SGAP/src/modelo/Condomino.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modelo;
 
 import java.util.Calendar;
@@ -13,10 +8,6 @@ import javax.persistence.Id;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-/**
- *
- * @author Borges
- */
 @Entity
 public class Condomino {
     @Id @GeneratedValue
